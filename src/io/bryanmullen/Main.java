8a95a4e61fac12fc43632e96f4dd3fa89167b231
Bryan Mullen <bryan@bryanmullen.me>
2022-02-07 19:57:50 +0000

    Initial Commit

---
+++ src/io/bryanmullen/Main.java
@@ -0,0 +1,22 @@
+package io.bryanmullen;
+
+//fix the code by figuring out what should be in the **** sections
+import javax.****.JOptionPane;
+public class EggBox{
+    public static void main(String[] args){
+        //variables
+        int eggs;
+        int boxSize;
+        int numBoxes;
+        int leftOverEggs;
+        //inputs
+		*****=Integer.****(JOptionPane.****(null,"Enter a number of eggs"));
+		*****=6;
+        //process
+        numBoxes=****;
+		****=eggs%boxSize;
+        //output
+        JOptionPane.****(null,"Number of boxes needed "+numBoxes);
+        JOptionPane.****(null,"Number of eggs left over "+leftOverEggs);
+    }
+}
